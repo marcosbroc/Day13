23fbd7f028c244dc5ce542b231d82ddec8abef1a
marcosbroc <mbroc@ontarget21.co.uk>
2025-08-24 18:24:34 +0200

    Compiler version changes for slither error

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.30;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
